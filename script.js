017e52064e97162636c2ab775bd9be45cccbca97
yurishe0 <yurishe00@gmail.com>
2023-01-03 21:54:20 +0100

    Add functionality to add books and show them on page

--- script.js
+++ script.js
@@ -7,63 +7,91 @@ const addBookWindow = document.querySelector(".add-book-window");
 const closeWindow = document.querySelector(".close");
 const submitBook = document.querySelector('button[type="submit"]');
 const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector('#author');
-const pagesInput = document.querySelector('#pages');
-const readInput = document.querySelector('#read');
-const libraryContainer = document.querySelector('.library');
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
+const libraryContainer = document.querySelector(".library");
 
 // Stores all created book objects.
 let library = [];
 
 // Book object constructor
 function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
 function checkBookCount() {
-    if(libraryContainer.children.length === 0) {
-        const message = document.createElement("p");
-        message.innerHTML = "You have no books in your library. Add some!";
-        libraryContainer.appendChild(message);
-    }
+  if (libraryContainer.children.length === 0) {
+    const message = document.createElement("p");
+    message.innerHTML = "You have no books in your library. Add some!";
+    libraryContainer.appendChild(message);
+  }
 }
 
 // Adds a book to the library list.
 function addToLibrary(book) {
-    library.push(book);
+  if (!this.title || !this.author || !this.pages) {
+    console.log("The fields can not be left empty!")
+  }
+
+  library.push(book);
+}
+
+function updateLibrary() {
+    libraryContainer.innerHTML = '';
+
+    library.forEach(book => {
+        const bookContainer = document.createElement("div");
+        bookContainer.classList.add("book");
+        libraryContainer.appendChild(bookContainer);
+
+        const title = document.createElement("h2");
+        title.innerHTML = book.title;
+
+        const author = document.createElement("h3");
+        author.innerHTML = book.author;
+
+        const pages = document.createElement("p");
+        pages.innerHTML = book.pages;
+
+        bookContainer.appendChild(title);
+        bookContainer.appendChild(author);
+        bookContainer.appendChild(pages);
+    })
 }
 
 function hideAddBookWindow() {
-    console.log("Clicked!");
-    mainWrapper.style.opacity = "1";
-    mainWrapper.style.pointerEvents = "auto";
-    addBookWindow.style.display = "none";
+  console.log("Clicked!");
+  mainWrapper.style.opacity = "1";
+  mainWrapper.style.pointerEvents = "auto";
+  addBookWindow.style.display = "none";
 }
 
 submitBook.addEventListener("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let userTitle = titleInput.value;
-    let userAuthor = authorInput.value;
-    let userpages = pagesInput.value;
-    let userRead = readInput.checked;
+  let userTitle = titleInput.value;
+  let userAuthor = authorInput.value;
+  let userpages = pagesInput.value;
+  let userRead = readInput.checked;
 
-    let book = new Book(userTitle, userAuthor, userpages, userRead);
-    addToLibrary(book);
+  let book = new Book(userTitle, userAuthor, userpages, userRead);
+  addToLibrary(book);
+  hideAddBookWindow();
 });
 
 closeWindow.addEventListener("click", hideAddBookWindow);
 
 newBook.addEventListener("click", () => {
-    // Dim the site
-    mainWrapper.style.opacity = "0.05";
-    // Disable clicking outside of the addBookWindow
-    mainWrapper.style.pointerEvents = "none";
-    // Display the adding book window
-    addBookWindow.style.display = "block";
+  // Dim the site
+  mainWrapper.style.opacity = "0.05";
+  // Disable clicking outside of the addBookWindow
+  mainWrapper.style.pointerEvents = "none";
+  // Display the adding book window
+  addBookWindow.style.display = "block";
 });
 
 checkBookCount();
